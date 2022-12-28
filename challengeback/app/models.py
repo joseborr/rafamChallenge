67f688433686cc2fac182fba5e39cfa77e11bb6c
jose <joseborr8@hotmail.com>
2022-12-28 12:36:25 -0300

    se configura bd postgres

---
+++ challengeback/app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(User):
+    pass
+
+class Courses(models.Model):
+    pass
+
+class Friendship(models.Model):
+    pass
--- challengeback/challengeback/settings.py
+++ challengeback/challengeback/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'challengeback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app',
+        'USER': 'app',
+        'PASSWORD': '!ChangeMe!',
+        'HOST': '127.0.0.1',
+        'PORT': '56706',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
